b1ed6227350e7733659f0bb7d3983f07398d0f0f
faris didin <dinzen2304@gmail.com>
2017-05-24 23:57:53 +0700

    login

---
+++ db_config.php
@@ -0,0 +1,8 @@
+<?php
+$host = "103.23.201.74";
+$user = "yzjvincb_mbd2017";
+$password = "luar.biasa";
+$datbase = "yzjvincb_netwars";
+$bd = mysqli_connect($host,$user,$password);
+mysqli_select_db($bd,$datbase);
+?>
